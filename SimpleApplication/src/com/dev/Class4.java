20deb4cff0ec5478e0aedb4ce15c0b370da3c894
view.cartoon <view.cartoon@gmail.com>
2019-02-24 21:23:36 +0700

    -

---
+++ SimpleApplication/src/com/dev/Class4.java
@@ -0,0 +1,22 @@
+package com.dev;
+
+public class Class4 {
+     private String tag1;
+     private String tag2;
+     
+	public String getTag1() {
+		return tag1;
+	}
+	public void setTag1(String tag1) {
+		this.tag1 = tag1;
+	}
+	public String getTag2() {
+		return tag2;
+	}
+	public void setTag2(String tag2) {
+		this.tag2 = tag2;
+	}
+     
+     
+     
+}
