e625776c8dd1ec80f5cbe359fea7564988efca53
view.cartoon <view.cartoon@gmail.com>
2019-02-24 19:46:02 +0700

    new Class2

---
+++ SimpleApplication/src/com/dev/Class2.java
@@ -0,0 +1,22 @@
+package com.dev;
+
+public class Class2 {
+     private int id;
+     private String remark;
+     
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+     
+     
+     
+}
